2c835d6915b0abede6e90df735591f1b5cc84418
weezzzt <wille1tap@outlook.com>
2023-09-19 11:05:22 +0200

    Lagt till functioner etc

---
+++ app.js
@@ -0,0 +1,36 @@
+const inputBox = document.getElementById("input-text");
+const listContainer = document.getElementById("list-container");
+const counter = document.getElementById("counter");
+let count = 0;
+
+
+
+function addTask() {
+    if (inputBox.value === "") {
+        alert("Du måste skriva något!");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        count++;
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+        counter.innerHTML = count;
+    }
+    inputBox.value = "";
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        count--;
+        counter.innerHTML = count;
+    }
+})
+
+listContainer.addEventListener("click", function(e) {
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+    }
+})
